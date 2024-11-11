6199a1c69503fac4f882979466f5e7972105f0a2
tiago do Carmo Cavalcanti <tiago123.ccavalcanti@gmail.com>
2024-11-10 22:24:04 -0300

    creatContact adjust

--- src/controllers/contactControllers/createContact.js
+++ src/controllers/contactControllers/createContact.js
@@ -4,7 +4,11 @@ require("../../models/associations.js");
 async function createContact(req, res) {
     try {
         const { personregisterId, phonenumber, email } = req.body;
-        const newContact = await Contact.create({ personregisterId, phonenumber, email });
+        const newContact = await Contact.create({ 
+            personregisterId, 
+            phonenumber, 
+            email 
+        });
         res.status(201).json(newContact);
     } catch (err) {
         res.status(500).json({ Error: "Error creating contact", details: err.message });
