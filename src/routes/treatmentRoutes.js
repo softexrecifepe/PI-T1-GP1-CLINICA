5338246cccbb0a09e368a627739a9fc38ffdde2b
Jônatas Talmon <jonatas_talmon@hotmail.com>
2024-11-23 18:37:41 -0300

    Fixes

--- src/routes/treatmentRoutes.js
+++ src/routes/treatmentRoutes.js
@@ -3,14 +3,13 @@ const router = express.Router();
 
 const treatmentControllers = require("../controllers/treatmentControllers/treatmentExports");
 
-// Defining routes for treatment CRUD operations
-router.post('/', treatmentControllers.createTreatment); // Create a new treatment
-router.get('/id/:treatmentId', treatmentControllers.readTreatmentById); // Read treatment by ID
-router.get('/pet/:petName', treatmentControllers.readTreatmentByPetName); // Read treatment by pet's name
-router.get('/tutor/:tutorName', treatmentControllers.readTreatmentByTutorName); // Read treatment by tutor's name
-router.get('/treatments', treatmentControllers.readAllTreatments); // Read all treatments
-router.put('/:treatmentId', treatmentControllers.updateTreatment); // Update an existing treatment
-router.delete('/:treatmentId', treatmentControllers.deleteTreatment); // Delete a treatment
+router.post('/', treatmentControllers.createTreatment); 
+router.get('/id/:treatmentId', treatmentControllers.readTreatmentById); 
+router.get('/pet/:petName', treatmentControllers.readTreatmentByPetName); 
+router.get('/tutor/:tutorName', treatmentControllers.readTreatmentByTutorName); 
+router.get('/treatments', treatmentControllers.readAllTreatments); 
+router.put('/:treatmentId', treatmentControllers.updateTreatment); 
+router.delete('/:treatmentId', treatmentControllers.deleteTreatment); 
 
-// Exporting the router for use in other parts of the application
+
 module.exports = router;
